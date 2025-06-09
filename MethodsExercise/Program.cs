0dcc7ef6df4b9085f0432ed755a0455fc2bb936b
Brian Yates <briany735@gmail.com>
2025-06-09 12:48:06 -0700

    C# method exercise 1 added

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+            Console.WriteLine(
+                $"{name} loves his pet {animal}, which is also called {name}. {name} is a {color} colored {animal} and loves listening to {band} with his owner.");
         }
     }
 }
